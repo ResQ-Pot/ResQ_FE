8f5a74c63544aeee4f44105588a3a5d37cf76e42
KimEunHye <rladmsgp031219@naver.com>
2026-03-17 10:34:07 +0900

    [FEAT/#3] 디자인 시스템 구현

---
+++ src/config/fonts.ts
@@ -0,0 +1,6 @@
+export const pretendard = {
+  'Pretendard-Bold': require('../../assets/fonts/Pretendard-Bold.otf'),
+  'Pretendard-SemiBold': require('../../assets/fonts/Pretendard-SemiBold.otf'),
+  'Pretendard-Medium': require('../../assets/fonts/Pretendard-Medium.otf'),
+  'Pretendard-Regular': require('../../assets/fonts/Pretendard-Regular.otf'),
+};
---
+++ src/config/tokens.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const raw = require('./tokens.js') as { colors: Tokens['colors'] };
+
+export interface Tokens {
+  colors: {
+    green: Record<50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900, string>;
+    status: Record<'safe' | 'warning' | 'danger', string>;
+    gray: Record<1 | 2 | 3 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13, string>;
+  };
+}
+
+export const colors = raw.colors;
